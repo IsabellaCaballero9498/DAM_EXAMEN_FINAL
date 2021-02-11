0569052c2d14104b64b5e07ad657c282b7b1686e
Piero Desposorio <pdesposoriogonzales@gmail.com>
2021-02-11 17:03:48 -0500

    Estudiante 3 Listo

--- app/src/main/java/com/dam/examen_final/Datos_Mapa.java
+++ app/src/main/java/com/dam/examen_final/Datos_Mapa.java
@@ -14,12 +14,12 @@ public class Datos_Mapa extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos__mapa);
         Button btnLogin;
-        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin = findViewById(R.id.btnEnviar);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //cambiar el 2do datos_mapa por el nombre de tu maps Activity
-                Intent i = new Intent(Datos_Mapa.this, Datos_Mapa.class);
+                Intent i = new Intent(Datos_Mapa.this, MAPS_ACTIVITY.class);
                 startActivity(i);
             }
         });
